4590f06bdf813b7857442a3e8dd06268ced0eb99
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-01-17 16:10:58 -0500

    start adding sections

--- TikkaIos/ScoreTableViewSource.cs
+++ TikkaIos/ScoreTableViewSource.cs
@@ -21,23 +21,68 @@ namespace TikkaIos
 
         public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
         {
-            var cell = tableView.DequeueReusableCell(CellIdentifier, indexPath) as ScoreTableViewCell;
-            cell.TextLabel.Text = $"index: {indexPath.Row}";
+            int section = indexPath.Section;
+            int row = indexPath.Row;
+            if(section == 0 && row == 0)
+            {
+                var titleCell = tableView.DequeueReusableCell("titleCell", indexPath);
+                if(titleCell == null)
+                {
+                    titleCell = new UITableViewCell();
+                }
 
-            var currentItem = items.ElementAt(indexPath.Row);
-            cell.TextLabel.Text = $"index: {currentItem.Index}, bid={currentItem.Bid}, recvd={currentItem.Received}";
+                titleCell.TextLabel.Text = "Tikka Score";
+                titleCell.UserInteractionEnabled = false;
+                return titleCell;
+            }
+            if (section == 1)
+            {
+                var cell = tableView.DequeueReusableCell(CellIdentifier, indexPath) as ScoreTableViewCell;
+                cell.TextLabel.Text = $"index: {indexPath.Row}";
 
-            return cell;
+                var currentItem = items.ElementAt(indexPath.Row);
+                cell.TextLabel.Text = $"index: {currentItem.Index}, bid={currentItem.Bid}, recvd={currentItem.Received}";
+
+                return cell;
+            }
+            else
+            {
+                // TODO
+                return new UITableViewCell();
+            }
         }
 
         public override nint RowsInSection(UITableView tableview, nint section)
         {
-            return items.Count;
+            if(section == 1)
+            {
+                return items.Count;
+            }
+            else
+            {
+                return 1;
+            }
         }
 
-        public override string TitleForHeader(UITableView tableView, nint section)
+        public override nint NumberOfSections(UITableView tableView)
         {
-            return "Tikka Score";
+            return 4;
         }
+        //public override string[] SectionIndexTitles(UITableView tableView)
+        //{
+        //    return new string [] { "One","Two","Three","Four" };
+        //}
+
+        //public override string TitleForHeader(UITableView tableView, nint section)
+        //{
+        //    if(section == 1)
+        //    {
+        //        return "Score";
+        //    }
+        //    else
+        //    {
+        //        return null;
+        //    }
+        //}
     }
 }
